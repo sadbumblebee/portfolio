0993e61150a255195a81fb68becafba01b1dea73
Daniel Wolfe <the.daniel.wolfe@gmail.com>
2020-05-26 14:16:51 -0700

    fix email and update npm packages

--- src/js/app.js
+++ src/js/app.js
@@ -7,19 +7,7 @@ var beenWarned = false;
 var hasPromisedToWorkWithMe = false;
 
 function init() {
-    emailLink();
     hm.run();
-}
-
-function emailLink() {
-    var link = document.querySelector('#email-link')
-
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        link.text('sadbumblebee at gmail dot com')
-        // Show email after email >   
-    })
-    
 }
 
 window.onload = () => init()
--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,16 +1,6 @@
-// NPM modules
-var _ = {};
-_.assign = require('lodash.assign');
-
-// var d3 = _.assign({},
-//   require("d3-selection"),
-//   require("d3-request")
-// );
-
-
 function init() {
-    // console.log('yup')
+    
 }
 
-init();
+window.onload = () => init();
 
